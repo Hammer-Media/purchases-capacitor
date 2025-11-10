28d0ee35cd28052e359aeeb5e8361c8a99de5ad4
Kevin Hallmark <khallmark@users.noreply.github.com>
2025-11-10 15:26:41 -0600

    Update Package.swift files to support iOS 15 and add purchases-hybrid-common dependency
    - Updated the iOS platform version from 14 to 15 in RevenuecatPurchasesCapacitor.
    - Added purchases-hybrid-common dependency in both RevenuecatPurchasesCapacitor and purchases-capacitor-ui.
    - Included PurchasesHybridCommon product in the dependencies for PurchasesPlugin and RevenueCatUIPlugin targets.

--- Package.swift
+++ Package.swift
@@ -3,21 +3,23 @@ import PackageDescription
 
 let package = Package(
     name: "RevenuecatPurchasesCapacitor",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "RevenuecatPurchasesCapacitor",
             targets: ["PurchasesPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0"),
+        .package(url: "https://github.com/RevenueCat/purchases-hybrid-common.git", from: "11.0.0")
     ],
     targets: [
         .target(
             name: "PurchasesPlugin",
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
-                .product(name: "Cordova", package: "capacitor-swift-pm")
+                .product(name: "Cordova", package: "capacitor-swift-pm"),
+                .product(name: "PurchasesHybridCommon", package: "purchases-hybrid-common")
             ],
             path: "ios/Sources/PurchasesPlugin"),
         .testTarget(
--- purchases-capacitor-ui/Package.swift
+++ purchases-capacitor-ui/Package.swift
@@ -10,7 +10,8 @@ let package = Package(
             targets: ["RevenueCatUIPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0"),
+        .package(url: "https://github.com/RevenueCat/purchases-hybrid-common.git", from: "11.0.0"),
     ],
     targets: [
         .target(
@@ -18,6 +19,7 @@ let package = Package(
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
                 .product(name: "Cordova", package: "capacitor-swift-pm"),
+                .product(name: "PurchasesHybridCommonUI", package: "purchases-hybrid-common"),
             ],
             path: "ios/Sources/RevenueCatUIPlugin"),
         .testTarget(
