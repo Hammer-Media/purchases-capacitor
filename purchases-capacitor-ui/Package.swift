a87b3fdccc2246d54d43f4f17b8228eb3bf61b13
Kevin Hallmark <khallmark@users.noreply.github.com>
2025-11-10 15:14:10 -0600

    Update package name casing in Package.swift for consistency

--- purchases-capacitor-ui/Package.swift
+++ purchases-capacitor-ui/Package.swift
@@ -2,11 +2,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "RevenuecatPurchasesCapacitorUI",
+    name: "RevenuecatPurchasesCapacitorUi",
     platforms: [.iOS(.v15)],
     products: [
         .library(
-            name: "RevenuecatPurchasesCapacitorUI",
+            name: "RevenuecatPurchasesCapacitorUi",
             targets: ["RevenueCatUIPlugin"])
     ],
     dependencies: [
